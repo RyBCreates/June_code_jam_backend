b9094739d5cfe80432ffb5bd8d37dc8071145e6d
Ryan B <bombingrun3@gmail.com>
2025-06-10 20:07:53 -0400

    Trying to Deploy Backend

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,7 +7,7 @@ const { loginUser, createUser } = require("./controllers/users");
 
 const app = express();
 
-const { PORT = 3001 } = process.env;
+const { PORT = 10000, DATABASE_URL } = process.env;
 
 app.use(express.json());
 app.use(cors());
@@ -16,10 +17,20 @@ app.post("/signin", loginUser);
 
 app.use("/", mainRouter);
 
-mongoose.connect("mongodb://127.0.0.1:27017/itinerarium_db");
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.listen(PORT, () => {
   console.log(`Hello, from Port: ${PORT}`);
 });
+
+if (!DATABASE_URL) {
+  console.error("Missing DATABASE_URL environment variable!");
+}
 
 module.exports = app;
